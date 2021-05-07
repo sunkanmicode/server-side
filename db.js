ae37b7899ba673d5ca17988e897ce7acab4a1620
sunkanmicode <oguntimehins@gmail.com>
2021-05-07 08:15:02 -0700

    editing database

--- db.js
+++ db.js
@@ -2,10 +2,8 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: 'postgres',
-    password: "postgresql-amorphous-60784",
-    host: 'localhost',
-    port: 5432,
-    database: 'fullstack'
+    password: process.env.DATABASE_URL,
+    ssl: true
 });
 
 module.exports = pool;
