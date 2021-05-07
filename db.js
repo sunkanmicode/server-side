34075eb1ae6b19d96b25425b823779ca3f95864e
sunkanmicode <oguntimehins@gmail.com>
2021-05-07 08:08:47 -0700

    adding database host address

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: 'postgres',
-    password: "oyebanjo87",
+    password: "postgresql-amorphous-60784",
     host: 'localhost',
     port: 5432,
     database: 'fullstack'
