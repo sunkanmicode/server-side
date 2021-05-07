33e54454ea7c21fabd9de6f590aac013b69ea658
sunkanmicode <oguntimehins@gmail.com>
2021-05-07 08:18:56 -0700

    editing datatbase

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    password: process.env.DATABASE_URL,
-    ssl: true
+    // user: 'postgres',
+    connectionString: "postgresql-amorphous-60784",
+    ssl:true,
 });
 
 module.exports = pool;
